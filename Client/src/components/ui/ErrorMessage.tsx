53e3bf05f98a3eb5ea219c53c385548a47cfef24
Rooqx <ashfaga2005f@gmail.com>
2025-07-15 01:33:10 +0100

    s

--- Client/src/components/ui/ErrorMessage.tsx
+++ Client/src/components/ui/ErrorMessage.tsx
@@ -17,7 +17,7 @@ export default function ErrorMessage({
 
   if (!message) return null;
   return (
-    <div className="fixed z-50 flex items-center gap-4 px-6 py-3 font-medium text-white rounded-lg shadow-lg bg-indigo-950 top-[70px] left-2 animate-fade-in rethink-sans">
+    <div className="fixed z-50 flex items-center gap-4 px-6 py-3 font-medium text-white rounded-lg shadow-lg bg-indigo-950 bottom-[70px] left-2 animate-fade-in rethink-sans">
       <div className="w-[10px] h-[35px] rounded-full -ml-2 bg-purple-700"></div>{" "}
       <span>{message}</span>
       {onClose && (
